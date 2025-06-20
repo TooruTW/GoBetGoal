ff63cad9751e931c10c5a4878718855d9f952700
Tooru Wu <tooruwu@ToorudeMacBook-Air.local>
2025-06-20 10:40:36 +0800

    feat-setting_shadcn_enviroment

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
+import TestingComponent from "./components/TestingComponent"
 
 
 function App() {
 
   return (
-    <section className="w-full min-h-screen flex justify-center items-center" >
+    <section className="w-full min-h-screen flex justify-center items-center flex-col" >
           <h1 className="text-9xl text-amber-50">Flag or bet</h1>
-
+          <TestingComponent />
     </section >
   )
 }
