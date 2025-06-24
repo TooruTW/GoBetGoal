e5b8f57d10fcd6f15f0920e8afc7588967a9363f
Tooru Wu <tooruwu@ToorudeMacBook-Air.local>
2025-06-24 16:38:01 +0800

    update-adujust-player_card_close_func

--- src/components/TrialDetail/DetailBoard/Participant/PlayerCard.tsx
+++ src/components/TrialDetail/DetailBoard/Participant/PlayerCard.tsx
@@ -1,9 +1,13 @@
+import React, { useRef } from "react";
 import { IoClose } from "react-icons/io5";
 import ImageLoader from "./ImageLoader";
 import type { Participant } from "@/components/types/Participant";
 
 interface acceptProps extends Participant {
-  handleDelete?: (id: string) => void;
+  handleDelete?: (
+    event: React.MouseEvent<SVGElement, MouseEvent>,
+    id: string
+  ) => void;
   handleAddFriend?: () => void;
 }
 
@@ -18,14 +22,16 @@ export default function PlayerCard(props: acceptProps) {
     handleAddFriend,
   } = props;
 
+  const isCloseAbleRef = useRef(playerName !== "unknown");
+
   return (
     <div className="group flex flex-col items-center gap-4 max-w-1/6 w-full bg-card-bg rounded-md py-6 ">
       <IoClose
         id={id}
-        onClick={() => {
-          handleDelete?.(id);
-        }}
-        className="self-end text-3xl mx-6 opacity-0 scale-0 group-hover:scale-100 group-hover:opacity-100 transition"
+        onClick={(event) => handleDelete?.(event, id)}
+        className={`self-end text-3xl mx-6 opacity-0 scale-0  group-hover:opacity-100 transition ${
+          isCloseAbleRef.current && "group-hover:scale-100"
+        }`}
       />
       <div className="h-65 w-full ">
         <ImageLoader imgUrl={playerImgUrl} />
@@ -42,12 +48,14 @@ export default function PlayerCard(props: acceptProps) {
           加好友
         </button>
       )}
-      <button
-        onClick={handleAddFriend}
-        className={`rounded-md bg-bg-tags text-black py-2 w-8/10`}
-      >
-        查看個人頁面
-      </button>
+      {isCloseAbleRef.current && (
+        <button
+          onClick={handleAddFriend}
+          className={`rounded-md bg-bg-tags text-black py-2 w-8/10`}
+        >
+          查看個人頁面
+        </button>
+      )}
     </div>
   );
 }
--- src/components/TrialDetail/DetailBoard/Participant/index.tsx
+++ src/components/TrialDetail/DetailBoard/Participant/index.tsx
@@ -2,14 +2,25 @@ import gsap from "gsap";
 import PlayerCard from "./PlayerCard";
 import { useEffect, useRef, useState } from "react";
 import type { Participant } from "@/components/types/Participant";
+import { IoClose } from "react-icons/io5";
 
-
-
-
-export default function Participant(props:{participant:Participant[]}) {
-    const {participant} = props
+export default function Participant(props: { participant: Participant[] }) {
+  const { participant } = props;
   const cardContainerRef = useRef<HTMLDivElement | null>(null);
-  const [list,setList] = useState(participant)
+  const [notice, setNotice] = useState<{
+    show: boolean;
+    x: number;
+    y: number;
+    id: string | null;
+    name: string;
+  }>({
+    show: false,
+    x: 0,
+    y: 0,
+    id: null,
+    name: "",
+  });
+  const [list, setList] = useState(participant);
 
   useEffect(() => {
     if (!cardContainerRef.current) return;
@@ -21,19 +32,33 @@ export default function Participant(props:{participant:Participant[]}) {
       {
         x: 0,
         duration: 0.5,
-        ease:"back",
+        ease: "back",
         stagger: 0.1,
       }
     );
   }, [cardContainerRef]);
 
-const handleDelete:(event:string)=>void = (event)=>{
-    setList((prev)=>{
-        return(
-            prev.filter((item)=>(item.id !== event))
-        )
-    })
-}
+  const handleDelete = (e: React.MouseEvent, id: string) => {
+    e.stopPropagation();
+    setNotice((prev) => ({
+      ...prev,
+      show: true,
+      x: e.clientX,
+      y: e.clientY,
+      id: id,
+      name: list.find((item) => item.id === id)?.playerName || "",
+    }));
+  };
+
+  const handleDeleteConfirm = (ans: boolean) => {
+    if (ans) {
+      if (notice.id) {
+        setList((prev) => prev.filter((item) => item.id !== notice.id));
+      }
+    }
+
+    setNotice((prev) => ({ ...prev, show: false, x: 0, y: 0, id: null }));
+  };
   return (
     <div ref={cardContainerRef} className="flex justify-between gap-4">
       {list.map((item) => {
@@ -61,6 +86,22 @@ const handleDelete:(event:string)=>void = (event)=>{
           />
         );
       })}
+      {/* confirm */}
+      {notice.show && (
+        <div
+          className="fixed z-50 bg-bg-notice rounded-md gap-2  p-4 flex flex-col"
+          style={{ left: notice.x, top: notice.y }}
+        >
+          <IoClose onClick={() => handleDeleteConfirm(false)} />
+          <span>確定要踢出 {notice.name} 嗎？</span>
+          <button
+            onClick={() => handleDeleteConfirm(true)}
+            className="bg-btn-notice rounded-md py-2 px-4 w-fit"
+          >
+            確定
+          </button>
+        </div>
+      )}
     </div>
   );
 }
