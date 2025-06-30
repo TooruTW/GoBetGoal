6f35914a321b2598116ae1c3d4fb610eb7ba9652
Tooru Wu <tooruwu@ToorudeMacBook-Air.local>
2025-06-30 17:04:34 +0800

    feat-swip_logic

--- src/components/TrialDetail/DetailBoard/ParticipantMobile/PlayerCard.tsx
+++ src/components/TrialDetail/DetailBoard/ParticipantMobile/PlayerCard.tsx
@@ -1,4 +1,6 @@
 import type { Participant } from "@/components/types/Participant";
+import { useSwipeable } from "react-swipeable";
+import { use, useEffect, useState } from "react";
 
 export default function PlayerCard(props: { participantInfo: Participant }) {
   const {
@@ -9,8 +11,46 @@ export default function PlayerCard(props: { participantInfo: Participant }) {
     friends = 0,
   } = props.participantInfo;
 
+  const [position, setPosition] = useState<number>(0);
+
+  const handlers = useSwipeable({
+    onSwiped: (eventData) => {
+      console.log("User Swiped!", eventData.deltaX);
+      if (eventData.deltaX > 0) {
+        if( eventData.deltaX > 50 && eventData.deltaX < 100){
+          setPosition(100)
+        }
+      }else{
+        if( eventData.deltaX < -50 && eventData.deltaX > -100){
+          setPosition(-100)
+        }
+      }
+    },
+    onSwiping: (eventData) => {
+      console.log("User Swiping!", eventData.deltaX);
+      if(eventData.deltaX > 0 ){
+        if (position > 100) return;
+        setPosition(eventData.deltaX);
+      } else {
+        if (position < -100) return;
+        setPosition(eventData.deltaX);
+      }
+        
+    },
+  });
+
+  useEffect(()=>{
+    if(position > -50 && position < 50){
+      setPosition(0)
+    }
+  },[position])
+
   return (
-    <div className="w-full h-20 flex gap-2 items-end justify-center relative">
+    <div
+      {...handlers}
+      className="w-full h-20 flex gap-2 items-end justify-center relative"
+      style={{ transform: `translateX(${position}px)` }}
+    >
       <div className="absolute bottom-0 left-0 w-full h-2/3 parallelogram-border z-0  "></div>
       <div className="w-full h-full z-20 flex justify-between items-center max-w-100">
         <img
@@ -18,22 +58,22 @@ export default function PlayerCard(props: { participantInfo: Participant }) {
           src={playerImgUrl}
           alt="playerImg"
         />
-          <h4 className ="self-end mb-4">{playerName}</h4>
-          <ul className="flex gap-2 self-end ">
-            <li className="flex flex-col items-center">
-              <span className="text-muted/50 font-medium">成功試煉數</span>
-              <span>{playerTotalTrials}</span>
-            </li>
-            <li className="flex flex-col items-center">
-              <span className="text-muted/50 font-">朋友數</span>
-              <span>{friends}</span>
-            </li>
-            <li className="flex flex-col items-center">
-              <span className="text-muted/50 font-">貼文讚數</span>
-              <span>{likedPosts}</span>
-            </li>
-          </ul>
-        </div>
+        <h4 className="self-end mb-4">{playerName}</h4>
+        <ul className="flex gap-2 self-end ">
+          <li className="flex flex-col items-center">
+            <span className="text-muted/50 font-medium">成功試煉數</span>
+            <span>{playerTotalTrials}</span>
+          </li>
+          <li className="flex flex-col items-center">
+            <span className="text-muted/50 font-">朋友數</span>
+            <span>{friends}</span>
+          </li>
+          <li className="flex flex-col items-center">
+            <span className="text-muted/50 font-">貼文讚數</span>
+            <span>{likedPosts}</span>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
