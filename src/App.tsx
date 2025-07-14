49b9ea2f6594cb4b8306dc849538784359fc4f0f
Tooru Wu <tooruwu@ToorudeMacBook-Air.local>
2025-07-14 17:39:15 +0800

    feat-store-userinfo-while-localstorage-exist

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,44 @@
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
-
-
+import { useSelector, useDispatch } from "react-redux";
+import { RootState } from "./state/store";
+import { useEffect, useState } from "react";
+import { useGetUserInfoSupa } from "./api";
+import { setAccount } from "./features/user/account";
 function App() {
+  const [userID, setUserID] = useState<string>("");
+
+  const userInfo = useSelector((state: RootState) => state.account);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const userID = localStorage.getItem("sb-rbrltczejudsoxphrxnq-auth-token");
+    if (userID) {
+      try {
+        const parsedData = JSON.parse(userID);
+        // 取出 userID
+        const userId = parsedData.user?.id;
+        setUserID(userId);
+      } catch (error) {
+        console.error("解析 localStorage 資料時發生錯誤:", error);
+      }
+    } else {
+      console.log("localStorage 中沒有找到 auth token");
+    }
+  }, []);
+
+  const {
+    data: userInfoSupa,
+    isLoading,
+    error,
+  } = useGetUserInfoSupa(userID, userID !== "");
+
+  useEffect(() => {
+    if (userID !== "" && !isLoading && !error) {
+      console.log("userInfo", userInfoSupa);
+      dispatch(setAccount(userInfoSupa));
+    }
+  }, [userID, userInfoSupa, userInfo, isLoading, error, dispatch]);
+
   return (
     <div className="text-amber-50 ">
       <Header />
--- src/state/store.ts
+++ src/state/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import trialsReducer from "@/features/trials/currentTrialSlice";
 import screenReducer from "@/features/user/currentScreenSize";
+import accountReducer from "@/features/user/account";
 export const store = configureStore({
   reducer: {
     trials: trialsReducer,
     screen: screenReducer,
+    account: accountReducer,
   },
 });
 
