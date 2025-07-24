0fb069a9bfa5131f189aef8bb94fde3c97856b82
Tooru Wu <tom123463@icloud.com>
2025-07-24 13:30:33 +0800

    feat(TrialComplete): submit button component

---
+++ src/components/pages/TrialComplete/components/MyTrialInfo.tsx
@@ -0,0 +1,3 @@
+export default function MyTrialInfo() {
+  return <div>MyTrialInfo</div>;
+}
--- src/components/pages/TrialComplete/index.tsx
+++ src/components/pages/TrialComplete/index.tsx
@@ -1,3 +1,13 @@
+import { Button } from "@/components/ui/button";
+
 export default function TrialComplete() {
-  return <div>TrialComplete</div>;
+  return (
+    <div className="flex flex-col gap-6 items-center w-full">
+      <div className="flex flex-col gap-6 items-center max-w-280 w-full border-2 border-schema-outline">
+        <div>upper</div>
+        <div>middle</div>
+        <Button className="w-full rounded-md text-p font-bold text-schema-on-primary">結算結果並分享到大平台</Button>
+      </div>
+    </div>
+  );
 }
