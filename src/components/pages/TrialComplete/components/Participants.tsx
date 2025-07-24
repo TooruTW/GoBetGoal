31d26df82634d720e9a76f66b4f216f7e54bcaa1
Tooru Wu <tom123463@icloud.com>
2025-07-24 17:00:27 +0800

    feat(OthersTrialInfo): add participants

--- src/components/pages/TrialComplete/components/Participants.tsx
+++ src/components/pages/TrialComplete/components/Participants.tsx
@@ -1,3 +1,12 @@
+import PlayerCard from "./PlayerCard";
+
 export default function Participants() {
-  return <div>participants</div>;
+  return (
+    <div className="flex flex-col gap-4 items-center w-1/3 min-w-100 translate-x-4">
+      <PlayerCard />  
+      <PlayerCard />  
+      <PlayerCard />  
+
+    </div>
+  );
 }
