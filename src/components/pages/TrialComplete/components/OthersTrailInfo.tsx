e510b21ffd9a0c18202042f1722cb27a96b49b86
Tooru Wu <tom123463@icloud.com>
2025-07-28 14:23:59 +0800

    feat(Participants): data insert

--- src/components/pages/TrialComplete/components/OthersTrailInfo.tsx
+++ src/components/pages/TrialComplete/components/OthersTrailInfo.tsx
@@ -1,10 +1,26 @@
 import Participants from "./Participants";
 import UploadImgs from "./UploadImgs";
+import { monsterDefault } from "@/assets/monster";
+
+const fakeParticipants = [
+  {
+    charactor_img_link: monsterDefault,
+    nick_name: "妖獸園長",
+    completeRate: "28/28",
+    cheatCount: 7,
+  },
+  {
+    charactor_img_link: monsterDefault,
+    nick_name: "妖獸園長",
+    completeRate: "28/28",
+    cheatCount: 7,
+  },
+];
 
 export default function OthersTrailInfo() {
   return (
     <div className="flex justify-between items-start gap-10 w-full p-6 max-xl:flex-col-reverse max-xl:gap-10 max-xl:max-h-none">
-      <Participants />
+      <Participants participants={fakeParticipants} />
       <UploadImgs />
     </div>
   );
