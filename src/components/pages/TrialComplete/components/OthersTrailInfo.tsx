e7ec34976651ecf962e78ceadfa9c52365686e4b
Tooru Wu <tom123463@icloud.com>
2025-07-28 14:26:49 +0800

    feat(UploadImages): data insert

--- src/components/pages/TrialComplete/components/OthersTrailInfo.tsx
+++ src/components/pages/TrialComplete/components/OthersTrailInfo.tsx
@@ -17,11 +17,22 @@ const fakeParticipants = [
   },
 ];
 
+const fakeImages = [
+  "/image/challengeSample/sample-1.jpg",
+  "/image/challengeSample/sample-2.jpg",
+  "/image/challengeSample/sample-3.jpg",
+  "/image/challengeSample/sample-1.jpg",
+  "/image/challengeSample/sample-2.jpg",
+  "/image/challengeSample/sample-3.jpg",
+  "/image/challengeSample/sample-1.jpg",
+  "/image/challengeSample/sample-2.jpg",
+];
+
 export default function OthersTrailInfo() {
   return (
     <div className="flex justify-between items-start gap-10 w-full p-6 max-xl:flex-col-reverse max-xl:gap-10 max-xl:max-h-none">
       <Participants participants={fakeParticipants} />
-      <UploadImgs />
+      <UploadImgs images={fakeImages} />
     </div>
   );
 }
--- src/components/pages/TrialComplete/components/UploadImgs.tsx
+++ src/components/pages/TrialComplete/components/UploadImgs.tsx
@@ -1,16 +1,13 @@
+interface acceptProps {
+  images: string[];
+}
 
-export default function UploadImgs() {
+export default function UploadImgs({images}:acceptProps) {
   return (
     <div className="grid grid-cols-4 gap-2 w-1/2 max-xl:w-full max-xl:flex max-xl:overflow-x-scroll max-xl:h-30">
-      <img className="aspect-square w-full object-cover rounded-md" src="/image/challengeSample/sample-1.jpg" alt="playerImage" />
-      <img className="aspect-square w-full object-cover rounded-md" src="/image/challengeSample/sample-2.jpg" alt="playerImage" />
-      <img className="aspect-square w-full object-cover rounded-md" src="/image/challengeSample/sample-3.jpg" alt="playerImage" />
-      <img className="aspect-square w-full object-cover rounded-md" src="/image/challengeSample/sample-1.jpg" alt="playerImage" />
-      <img className="aspect-square w-full object-cover rounded-md" src="/image/challengeSample/sample-2.jpg" alt="playerImage" />
-      <img className="aspect-square w-full object-cover rounded-md" src="/image/challengeSample/sample-3.jpg" alt="playerImage" />
-      <img className="aspect-square w-full object-cover rounded-md" src="/image/challengeSample/sample-1.jpg" alt="playerImage" />
-      <img className="aspect-square w-full object-cover rounded-md" src="/image/challengeSample/sample-2.jpg" alt="playerImage" />
-
+      {images.map((image,index)=>(
+        <img key={index} className="aspect-square w-full object-cover rounded-md" src={image} alt="playerImage" />
+      ))}
     </div>
   );
 }
