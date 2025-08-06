2772db1816b583cdced5e602c71d33d5c3e28547
Tooru Wu <tom123463@icloud.com>
2025-08-06 16:34:44 +0800

    feat:selected animation

--- src/components/pages/Trials/components/TrialDetail/DetailBoard/Participant/Invitition.tsx
+++ src/components/pages/Trials/components/TrialDetail/DetailBoard/Participant/Invitition.tsx
@@ -1,31 +1,20 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { UserInfoSupa } from "@/types/UserInfoSupa";
-import { monsterDefault } from "@/assets/monster/index.ts";
 import { Button } from "@/components/ui/button";
 import { useSelector } from "react-redux";
 import { RootState } from "@/store";
 import { useParams } from "react-router-dom";
 import { useTrialSupa } from "@/api";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 type acceptProps = {
   className?: string;
 };
-const fakeData = {
-  candy_count: 0,
-  charactor_img_link: monsterDefault,
-  cheat_blanket: 0,
-  nick_name: "朋友1",
-  system_preference_color_mode: "light",
-  total_trial_count: 0,
-  liked_posts_count: 0,
-  friend_count: 0,
-  user_id: "1",
-  purchase_challenge: [],
-  purchase_avatar: [],
-};
 
 export default function Invitition({ className }: acceptProps) {
   const [invititionList, setInvititionList] = useState<UserInfoSupa[]>([]);
+  const [selectedInvitition, setSelectedInvitition] = useState<string[]>([]);
 
   const { id } = useParams();
   const { data: trial, isLoading, error } = useTrialSupa(id as string);
@@ -44,17 +33,71 @@ export default function Invitition({ className }: acceptProps) {
     setInvititionList(friendNotInPlayerSet);
   }, [friendList, id, isLoading, error, trial]);
 
+  const invititionListRef = useRef<HTMLDivElement>(null);
+
+  useGSAP(
+    () => {
+      if (invititionList.length === 0) return;
+      gsap.from(".avatar", {
+        delay: 1,
+        scale: 0,
+        duration: 1,
+        rotation: 360,
+        ease: "power2.inOut",
+        stagger: 0.1,
+      });
+    },
+    { dependencies: [invititionList], scope: invititionListRef }
+  );
+  useGSAP(
+    () => {
+      if (friendList.length === 0) return;
+      if (selectedInvitition.length < invititionList.length) {
+        gsap.to(".unselected", {
+          duration: 0.1,
+          filter: "brightness(0.9)",
+          fontWeight: "normal",
+          x: 0,
+          borderBottom: "none",
+        });
+      }
+      if (selectedInvitition.length > 0) {
+        gsap.to(".selected", {
+          duration: 0.1,
+          filter: "brightness(1)",
+          fontWeight: "extrabold",
+          x: 25,
+          borderBottom: "5px solid var(--color-schema-outline)",
+        });
+      }
+    },
+    { dependencies: [selectedInvitition], scope: invititionListRef }
+  );
+
+  const handleSelect = (userId: string) => {
+    if (selectedInvitition.includes(userId)) {
+      setSelectedInvitition(selectedInvitition.filter((id) => id !== userId));
+    } else {
+      setSelectedInvitition([...selectedInvitition, userId]);
+    }
+  };
+
   return (
     <div
       className={`${className} backdrop-blur-xs bg-schema-surface-container-high/50 flex flex-col items-center justify-center`}
     >
-      <div>
+      <div ref={invititionListRef}>
         <h2 className="text-h2">邀請列表</h2>
-        <ul className="flex flex-col bg-schema-surface-container rounded-md py-8">
+        <ul className="flex flex-col bg-schema-surface-container rounded-md py-8 pr-10">
           {invititionList.map((item) => (
             <li
               key={item.user_id}
-              className="flex items-center bg-schema-surface-container-high justify-between gap-4 px-4 py-2 border-b border-schema-outline"
+              className={`flex items-center justify-between gap-4 px-4 pt-4 active:scale-95 cursor-pointer brightness-90 ${
+                selectedInvitition.includes(item.user_id)
+                  ? "selected"
+                  : "unselected"
+              }`}
+              onClick={() => handleSelect(item.user_id)}
             >
               <div
                 style={{
@@ -62,13 +105,12 @@ export default function Invitition({ className }: acceptProps) {
                   backgroundSize: "160%",
                   backgroundPosition: "top",
                   backgroundRepeat: "no-repeat",
-                  backgroundColor: "white",
                 }}
-                className="w-15 aspect-square rounded-full"
+                className="w-15 aspect-square avatar"
               ></div>
 
               <ul className="grid grid-cols-3 gap-2 w-full">
-                <li className="text-h3 text-center    ">{item.nick_name}</li>
+                <li className="text-h3 text-center">{item.nick_name}</li>
                 <li className="text-h3">完成試煉：{item.total_trial_count}</li>
                 <li className="text-h3">熱門貼文：{item.liked_posts_count}</li>
               </ul>
