fd2b3eb1ad3aa1c1e4ec7d92234eaad5a193d8ca
Tooru Wu <tom123463@icloud.com>
2025-08-06 09:23:36 +0800

    fex(bagel): fix case issue

---
+++ src/assets/bagel/index.ts
@@ -0,0 +1,9 @@
+// Candy 圖片資源
+import bagel1 from "./temp_bagel1.webp";
+import bagel2 from "./temp_bagel2.webp";
+import bagel3 from "./temp_bagel3.webp";
+import bagel4 from "./temp_bagel4.webp";
+
+export { bagel1, bagel2, bagel3, bagel4 };
+
+export type BagelImage = typeof bagel1;
--- src/assets/candy/index.ts
+++
@@ -1,9 +0,0 @@
-// Candy 圖片資源
-import bagel1 from "./Bagel1.webp";
-import bagel2 from "./bagel2.webp";
-import bagel3 from "./bagel3.webp";
-import bagel4 from "./bagel4.webp";
-
-export { bagel1, bagel2, bagel3, bagel4 };
-
-export type BagelImage = typeof bagel1;
