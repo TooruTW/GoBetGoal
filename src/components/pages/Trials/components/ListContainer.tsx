4a0b1c19d2690d8d2185aad7577ab42ee862dcd5
Tooru Wu <tom123463@icloud.com>
2025-08-11 15:29:26 +0800

    feat:filt by scope

--- src/components/pages/Trials/components/ListContainer.tsx
+++ src/components/pages/Trials/components/ListContainer.tsx
@@ -1,20 +1,33 @@
 import TrialCard from "./TrialCard";
 import { useTrialAllSupa } from "@/api";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import GlareHover from "@/components/shared/reactBit/GlareHover";
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { RootState } from "@/store";
+import { TrialSupa } from "@/types/TrialSupa";
 
 export default function ListContainer() {
   const { data, isLoading, error } = useTrialAllSupa();
+  const { scope, category } = useParams();
+  const [filteredData, setFilteredData] = useState<TrialSupa[]>([]);
+  const userId = useSelector((state: RootState) => state.account.user_id);
 
   useEffect(() => {
-    if (isLoading) return;
+    if (isLoading || !data || !userId) return;
     if (error) console.log(error);
-    console.log(data);
-  }, [data, isLoading, error]);
+    if(scope === "my"){
+      const filteredData = data.filter((trial) => trial.trial_participant.some((participant) => participant.user_info.user_id === userId));      
+      setFilteredData(filteredData);
+    }else{
+      setFilteredData(data);
+    }
+
+  }, [data, isLoading, error, scope, category, userId]);
 
   return (
     <div className="w-full px-3 grid md:grid-cols-2 gap-6 relative z-20">
-      {data?.map((trial) => (
+      {filteredData?.map((trial) => (
         <GlareHover key={trial.id} className="bg-surface-container">
           <TrialCard trial={trial} />
         </GlareHover>
