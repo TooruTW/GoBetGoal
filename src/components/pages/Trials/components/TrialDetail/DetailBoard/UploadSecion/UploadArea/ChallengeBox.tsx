0f0acb6443d49b400edd4e308b1936b5320b5594
Tooru Wu <tom123463@icloud.com>
2025-08-12 17:07:20 +0800

    update(challengeBox): add upload card layout

--- src/components/pages/Trials/components/TrialDetail/DetailBoard/UploadSecion/UploadArea/ChallengeBox.tsx
+++ src/components/pages/Trials/components/TrialDetail/DetailBoard/UploadSecion/UploadArea/ChallengeBox.tsx
@@ -1,14 +1,20 @@
 import { Button } from "@/components/ui/button";
 import { TrialDetailSupa } from "@/types/TrialDetailSupa";
 import { useEffect } from "react";
+import { IoAddOutline } from "react-icons/io5";
 
 export default function ChallengeBox({
   currentChallenge,
 }: {
   currentChallenge: TrialDetailSupa;
 }) {
-  const { stage_index, start_at, challenge_stage, chance_remain } =
-    currentChallenge;
+  const {
+    stage_index,
+    start_at,
+    challenge_stage,
+    chance_remain,
+    upload_image,
+  } = currentChallenge;
 
   useEffect(() => {
     console.log(currentChallenge, "currentChallenge");
@@ -28,17 +34,26 @@ export default function ChallengeBox({
           </span>
         </Button>
       </div>
-      <div className="flex justify-center items-center rounded-md h-full gap-2">
+      <div className="flex justify-center items-center rounded-md h-full gap-2 max-h-65">
         {challenge_stage.description.map((item, index) => {
           return (
             <div
               key={index}
-              className="border-2 border-schema-primary rounded-md w-full max-w-1/2 h-full"
+              className="border-2 border-schema-primary rounded-md w-full max-w-1/2 h-full "
             >
-              <div className="w-full h-1/4 bg-schema-primary flex items-center justify-center text-schema-on-primary p-1">
+              <div className="w-full h-1/5 bg-schema-primary flex items-center justify-center text-schema-on-primary p-1">
                 {item}
               </div>
-              <div className="w-full h-3/4"></div>
+              <div className="w-full h-4/5 flex items-center justify-center border-2 border-schema-primary relative">
+                {!upload_image && <IoAddOutline className="size-20 absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 text-schema-primary" />}
+                {!upload_image && (
+                  <img
+                    src={challenge_stage.sample_image[index]}
+                    alt="sample"
+                    className="w-full h-full object-contain opacity-50"
+                  />
+                )}
+              </div>
             </div>
           );
         })}
