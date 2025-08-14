47e3cc6670460f6678f63039a2ab690f3d39faa0
sunny2001522 <seliya512399@gmail.com>
2025-08-14 18:30:03 +0800

    feat: get all avatar api

---
+++ src/api/getAvatarStats.ts
@@ -0,0 +1,23 @@
+import { supabase } from "@/supabaseClient";
+import { useQuery } from "@tanstack/react-query";
+
+export const getAvatarStats = async () => {
+  const { data: user_info, error } = await supabase
+    .from("user_info")
+    .select("charactor_img_link");
+    
+  if (error) {
+    throw new Error(error.message);
+  }
+
+  return user_info;
+};
+
+export function useGetAvatarStats() {
+  const { data, error, isLoading } = useQuery({
+    queryKey: ["avatar_stats"],
+    queryFn: () => getAvatarStats(),
+  });
+
+  return { data, error, isLoading };
+}
