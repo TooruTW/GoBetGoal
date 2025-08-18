11f1ae815e6d09b2f677ce9ec0a8243ead803397
Tooru Wu <tom123463@icloud.com>
2025-08-18 11:40:56 +0800

    feat(uploadsection):add swipe animation

--- src/components/pages/Trials/components/TrialDetail/DetailBoard/UploadSecion/UploadArea/index.tsx
+++ src/components/pages/Trials/components/TrialDetail/DetailBoard/UploadSecion/UploadArea/index.tsx
@@ -1,8 +1,10 @@
 import { GrFormNext, GrFormPrevious } from "react-icons/gr";
 
 import ChallengeBox from "./ChallengeBox";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { TrialDetailSupa } from "@/types/TrialDetailSupa";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 type acceptProps = {
   trial: TrialDetailSupa[];
@@ -17,14 +19,31 @@ export default function UploadArea({
 }: acceptProps) {
   const [currentChallenge, setCurrentChallenge] =
     useState<TrialDetailSupa | null>(null);
+  const [previousIndex, setPreviousIndex] = useState<number>(0);
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  const { contextSafe } = useGSAP();
+
+  const removeAnimation = contextSafe((isNext: boolean) => {
+    if (!containerRef.current) return;
+    const newIndex = isNext ? currentIndex + 1 : currentIndex - 1;
+    gsap.to(containerRef.current, {
+      x: isNext ? -100 : 100,
+      duration: 0.25,
+      opacity: 0,
+      onComplete: () => {
+        setCurrentIndex(newIndex);
+      },
+    });
+  });
 
   const handleSwitch = (isNext: boolean) => {
     if (isNext) {
       if (currentIndex === trial.length) return;
-      setCurrentIndex(currentIndex + 1);
+      removeAnimation(true);
     } else {
       if (currentIndex === 1) return;
-      setCurrentIndex(currentIndex - 1);
+      removeAnimation(false);
     }
   };
 
@@ -34,9 +53,40 @@ export default function UploadArea({
     setCurrentChallenge(challenge);
   }, [currentIndex, trial]);
 
+  // 追蹤前一個索引
+  useEffect(() => {
+    if (currentIndex !== previousIndex) {
+      setPreviousIndex(currentIndex);
+    }
+  }, [currentIndex, previousIndex]);
+
+  useGSAP(
+    () => {
+      // 根據前一個數字決定動畫方向
+      const isNext = currentIndex > previousIndex;
+      console.log(isNext);
+      const startX = isNext ? 100 : -100; // 如果是下一個，從右邊進入；如果是上一個，從左邊進入
+
+      gsap.fromTo(containerRef.current, {
+        x: startX,
+        opacity: 0,
+      }, {
+        x: 0,
+        duration: 0.5,
+        opacity: 1,
+        ease: "power2.inOut",
+      });
+    },
+    { dependencies: [currentIndex], revertOnUpdate: true }
+  ); // 依賴於這兩個值
+
   return (
-    <div className="h-full w-full relative max-xl:bg-schema-surface-container-high max-xl:p-2 max-xl:rounded-2xl">
-      {currentChallenge && <ChallengeBox currentChallenge={currentChallenge} />}
+    <div className="h-full w-full relative max-xl:bg-schema-surface-container-high max-xl:p-2 max-xl:rounded-2xl overflow-hidden">
+      <div className="w-full h-full" ref={containerRef}>
+        {currentChallenge && (
+          <ChallengeBox currentChallenge={currentChallenge} />
+        )}
+      </div>
       <GrFormNext
         className="size-10 hover:bg-schema-outline rounded-full -translate-y-1/2 absolute top-1/2 -right-2"
         onClick={() => handleSwitch(true)}
