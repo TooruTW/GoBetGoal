3f75b7a04d11033ae2f556edbdf4082661a050be
sunny2001522 <seliya512399@gmail.com>
2025-08-22 11:52:01 +0800

    feat(Home):award list

---
+++ src/components/pages/Home/MainMachine/component/AwardList.tsx
@@ -0,0 +1,28 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import Award from "./award.tsx";
+
+export default function AwardList() {
+  return (
+    <Tabs
+      defaultValue="successAward"
+      className="flex flex-col  justify-center  items-center  w-full px-3 max-w-330 h-dvh mt-16"
+    >
+      <TabsList className="flex justify-center mb-4 w-full max-w-100">
+        <TabsTrigger value="successAward">試煉高手榜</TabsTrigger>
+        <TabsTrigger value="trialAward">越戰越勇榜</TabsTrigger>
+      </TabsList>
+
+      {/* 註冊 */}
+      <TabsContent value="successAward">
+        {" "}
+        <Award />
+      </TabsContent>
+
+      {/* 登入 */}
+      <TabsContent value="trialAward">
+        {" "}
+        <Award />
+      </TabsContent>
+    </Tabs>
+  );
+}
