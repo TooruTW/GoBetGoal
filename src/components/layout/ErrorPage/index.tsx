1766d92fa576d5c979274f39499b28b7618a9679
sunny2001522 <seliya512399@gmail.com>
2025-08-26 00:25:28 +0800

    feat:add Error Page

--- src/components/layout/ErrorPage/index.tsx
+++ src/components/layout/ErrorPage/index.tsx
@@ -1,33 +1,9 @@
-import { Button } from "@/components/ui/button";
-import gsap from "gsap";
-import { RefObject, useEffect, useRef } from "react";
-import { Link } from "react-router-dom";
+import Game from "./Game.tsx";
 
 export default function ErrorPages() {
-  const titleRef: RefObject<HTMLHeadingElement | null> = useRef(null);
-  useEffect(() => {
-    if (!titleRef.current) return;
-    gsap.fromTo(
-      titleRef.current,
-      {
-        y: -100,
-      },
-      {
-        y: 0,
-        duration: 1,
-        ease:"bounce"
-      }
-    );
-  }, []);
-
   return (
     <div className="w-full h-screen flex flex-col gap-4 justify-center items-center">
-      <h1 ref={titleRef} className="text-amber-50 text-6xl">
-        Error happening
-      </h1>
-      <Button variant="destructive">
-        <Link to="/">Redirect</Link>
-      </Button>
+      <Game />
     </div>
   );
 }
