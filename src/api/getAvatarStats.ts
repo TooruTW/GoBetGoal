1c42f8f2af7c9d837ea6444082ea7a7785e611ac
Tooru Wu <tom123463@icloud.com>
2025-08-28 15:22:53 +0800

    fix:build bugs char case issue

--- src/api/getAvatarStats.ts
+++ src/api/getAvatarStats.ts
@@ -5,11 +5,9 @@ export const getAvatarStats = async () => {
   const { data: user_info, error } = await supabase
     .from("user_info")
     .select("character_img_link");
-
   if (error) {
     throw new Error(error.message);
   }
-
   return user_info;
 };
 
