55d3602ef12cb1cb014c69f3824d4101a046323b
sunny2001522 <seliya512399@gmail.com>
2025-09-05 16:02:39 +0800

    update(Dev):ui

--- src/components/pages/DevPage/index.tsx
+++ src/components/pages/DevPage/index.tsx
@@ -1,13 +1,13 @@
 import DevSideBar from "./components/DevSideBar";
-import SlotMachine from "../Home/MainMachine/component/SlotMachine";
+import { Outlet } from "react-router-dom";
 
 export default function DevPage() {
   return (
     <div className="w-full min-h-screen">
       <div className="flex">
         <DevSideBar />
-        <section className="w-full flex flex-col gap-10 justify-center items-center h-screen">
-          <SlotMachine />
+        <section className="w-full ps-14 flex flex-col gap-10 justify-center items-center  mt-10">
+          <Outlet />
         </section>
       </div>
     </div>
