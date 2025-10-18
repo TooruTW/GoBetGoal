f60352bdf0d13c6744f4d8f80976ef1b1cacdd8a
Tooru Wu <tom123463@icloud.com>
2025-10-18 15:50:38 +0800

    chore: update Supabase configuration to use environment variables

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -2,8 +2,8 @@
 
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://rbrltczejudsoxphrxnq.supabase.co";
-const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJicmx0Y3planVkc294cGhyeG5xIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE2MTkyODcsImV4cCI6MjA2NzE5NTI4N30.FhwbiJoQ4sUKM8ep4jDTBP2OtfZrJ7DgB2NHf6X-BTU";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
